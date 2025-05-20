1aaed5a7175dddfa66297135baf42041aa71f7e3
currysoda <currysoda1598@gmail.com>
2025-05-20 23:47:29 +0900

    메소드 참조 작성 중

--- src/methodreferences/Main.java
+++ src/methodreferences/Main.java
@@ -1,10 +1,26 @@
 package methodreferences;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		List<Integer> list1 = new ArrayList<>();
+		
+		list1.add(1);
+		list1.add(3);
+		list1.add(5);
+		list1.add(7);
+		list1.add(9);
+		
+		Consumer<List<Integer>> c = l -> {
+			Collections.reverse(l);
+		};
+		
 		
 	}
 }
